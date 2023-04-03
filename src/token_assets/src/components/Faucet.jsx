d9e8fd8cff30a316077580c285902a303b3d9db9
Pedro Pinho <96132977+Pinho17@users.noreply.github.com>
2023-04-03 17:54:38 +0100

    Added src folder

---
+++ src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { token, canisterId, createActor } from "../../../declarations/token";
+import { AuthClient } from "../../../../node_modules/@dfinity/auth-client/lib/cjs/index";
+
+function Faucet() {
+
+  const [isDisabled, setDisabled] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
+
+  async function handleClick(event) {
+    setDisabled(true);
+
+    const authClient = await AuthClient.create();
+    const identity = await authClient.getIdentity();
+
+    const authenticatedCanister = createActor(canisterId, {
+      agentOptions: {
+        identity,
+      },
+    });
+
+    const result = await authenticatedCanister.payOut();
+    setText(result);
+  }
+
+  return (
+    <div className="blue window">
+      <h2>
+        <span role="img" aria-label="tap emoji">
+          🚰
+        </span>
+        Faucet
+      </h2>
+      <label>Get your free PING tokens here! Claim 10,000 PING tokens to your account.</label>
+      <p className="trade-buttons">
+        <button id="btn-payout" onClick={handleClick} disabled={isDisabled}>
+          {buttonText}
+        </button>
+      </p>
+    </div>
+  );
+}
+
+export default Faucet;
